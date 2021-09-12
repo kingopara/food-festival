7de18fe2ffcf31cfb385543ec235e334c4a094db
praiseopara <cpopara2010@yahoo.com>
2021-09-12 05:20:18 -0500

    converting to pwa

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const webpack = require("webpack");
 const path = require("path");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
 
 module.exports = {
     entry: {
@@ -17,7 +19,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.jpg$/i,
+                test: /\.(png|jpe?g|gif)$/i,
                 use: [
                     {
                         loader: "file-loader",
@@ -45,6 +47,21 @@ module.exports = {
         }),
         new BundleAnalyzerPlugin({
             analyzerMode: "static", // the report outputs an HTML file in the dist folder
+        }),
+        new WebpackPwaManifest({
+            name: "Food Event",
+            short_name: "Foodies",
+            description: "An app that allows you to view upcoming food events.",
+            start_url: "../index.html",
+            background_color: "#01579b",
+            theme_color: "#ffffff",
+            fingerprints: false,
+            inject: false,
+            icons: [{
+                src: path.resolve("assets/img/icons/icon-512x512.png"),
+                sizes: [96, 128, 192, 256, 384, 512],
+                destination: path.join("assets", "icons")
+            }]
         })
     ],
     mode: 'development'
