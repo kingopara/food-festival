00b20f232ae0e46a2cf3f89f257a84f81c9cd6a9
praiseopara <cpopara2010@yahoo.com>
2021-09-11 20:20:53 -0500

    fixed technical issues, registered & installed & activated service worker and intercept fetch requests

--- service-worker.js
+++ service-worker.js
@@ -16,11 +16,50 @@ const FILES_TO_CACHE = [
     "./dist/schedule.bundle.js"
 ];
 
+// install service worker
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
             console.log('installing cache : ' + CACHE_NAME)
             return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
+
+// activate service worker
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeeplist.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i] );
+                    return caches.delete(keyList[i]);
+                }
+            }))
+        })
+
+    )
+})
+
+// fetch event
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) { //if cache is available, respond with cache
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else { //if no cache, fetch request
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+            // You can omit if/else for console.log & put one line below like this too.
+            // return request || fetch(e.request)
         })
     )
 })
